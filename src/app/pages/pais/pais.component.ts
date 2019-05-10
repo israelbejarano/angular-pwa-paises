4a3487ece42334de1e66776d3d1ee4a1a2acaf06
israel <israel.bejarano.a@gmail.com>
2019-05-10 23:03:19 +0200

    diseño final del componente

--- src/app/pages/pais/pais.component.ts
+++ src/app/pages/pais/pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisesService } from '../../services/paises.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaisInterface } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-pais',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PaisComponent implements OnInit {
+  pais: PaisInterface;
 
-  constructor() { }
+  constructor(public paisesService: PaisesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.paisesService.getPaisById(id).then(pais => {
+      if (!pais) {
+        return this.router.navigateByUrl('/');
+      }
+      this.pais = pais;
+    });
   }
 
 }
